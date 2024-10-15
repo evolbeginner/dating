914152e56deca3c8059013ffb7b26709ad2cd997
evolbeginner <sishuowang@hotmail.ca>
2024-10-15 14:17:23 +0800

    Test

--- mcmc3r/calculate_marginal_likelihood.sh
+++ mcmc3r/calculate_marginal_likelihood.sh
@@ -9,17 +9,23 @@ wd=`pwd`
 for i in `ls`; do
 	[ ! -d "$i" ] && continue
 	cd $i/date/mcmc3r
-	logmls=()
+	logmls=(); ses=()
+	echo -ne "$i\t"
 	for t in AR IR; do
 		cd $t
-		logml=`Rscript ~/project/Rhizobiales/scripts/dating/mcmc3r/calculate_mcmc3r_logml.R 2>/dev/null`
+		#cmd="Rscript ~/project/Rhizobiales/scripts/dating/mcmc3r/calculate_mcmc3r_logml.R 2>/dev/null"
+		logml=`Rscript ~/project/Rhizobiales/scripts/dating/mcmc3r/calculate_mcmc3r_logml.R 2>/dev/null | awk '{print $1}'`
 		logmls=(${logmls[@]} $logml)
+		se=`Rscript ~/project/Rhizobiales/scripts/dating/mcmc3r/calculate_mcmc3r_logml.R 2>/dev/null | awk '{print $2}'`
+		ses=(${ses[@]} $se)
 		cd - >/dev/null
 	done
 	logml_diff=`echo -e "scale=3; ${logmls[0]} - ${logmls[1]}" | bc`
 	ratio=`echo -e "e($logml_diff)" | bc -l`
 	marginal_lh=`echo -e "scale=3; $ratio/($ratio+1)" | bc` # AR
-	echo -e "$logml_diff\t$marginal_lh"
+	echo -ne "$logml_diff\t$marginal_lh\t"
+	echo -ne "${logmls[@]}\t" | tr ' ' '\t'
+	echo "${ses[@]}" | tr ' ' '\t'
 	cd $wd >/dev/null
 done
 
--- mcmc3r/calculate_mcmc3r_logml.R
+++ mcmc3r/calculate_mcmc3r_logml.R
@@ -2,5 +2,6 @@ library(mcmc3r)
 
 clk <- mcmc3r::stepping.stones()
 
-cat(clk$logml)
+#cat(clk$logml)
+cat(paste(c(clk$logml,clk$se), "\t"))
 cat("\n")
